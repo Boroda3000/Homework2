92a980613ed8a7d04144fb3d2c5c81920a3d98d7
Boroda3000 <pavluk.a.m@yandex.ru>
2024-07-21 21:46:14 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,4 @@
+number_of_homework = 12
+number_of_hours = 1.5
+cource_name = "Python"
+print("Курс:",cource_name,",всего задач:",number_of_homework,",затрачено часов:",number_of_hours,",среднее время выполнения задачи:",number_of_hours/number_of_homework)
